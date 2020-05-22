8993c9f418c656c38e7bb26b22f5091b1bf6a242
nodokaodayaka <wankosob@gmail.com>
2020-05-22 15:14:56 +0900

    Updates

---
+++ precache-manifest.2cb7f56f7374f6939f3a61a94dd0bf90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed274f3a0367f2824a644ce995c5f39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa7c5dee3efd5e7ed9be",
+    "url": "./static/css/main.f4bc1fdf.chunk.css"
+  },
+  {
+    "revision": "4cf4280835c2df607f0f",
+    "url": "./static/js/2.267c16d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.267c16d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa7c5dee3efd5e7ed9be",
+    "url": "./static/js/main.cfbf600f.chunk.js"
+  },
+  {
+    "revision": "d6013b911e9a743fea1f",
+    "url": "./static/js/runtime-main.5f367691.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
